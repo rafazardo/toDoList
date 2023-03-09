754ca1c9249397c096b91e00f7e8ef192647f807
Rafael Zardo <rafael.zardo@outlook.com>
2023-03-09 16:03:12 -0300

    Botão de deletar items adicionado

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -6,6 +6,16 @@ class TodoItemsController < ApplicationController
         redirect_to @todo_list
     end
 
+    def show
+        @todo_item = @todo_list.todo_items.find(params[:id])
+        if @todo_item.destroy 
+            flash[:sucess] = "O item foi deletado da lista"
+        else 
+            flash[:error] = "Erro O item não foi deletado da lista"
+        end
+        redirect_to todo_list_path(@todo_list)
+    end
+
     private 
 
     def set_todo_list
@@ -15,4 +25,5 @@ class TodoItemsController < ApplicationController
     def todo_items_params
         params[:todo_item].permit(:conteudo)
     end
+    
 end
